43ecabf868a9d86479d08eb42743f188c51f1346
MaevaGilles <maevagilles-web@hotmail.fr>
2022-10-03 15:28:36 +0200

    exo of the day

---
+++ my_first_program/my_fourth_program.rb
@@ -0,0 +1,3 @@
+print "Bonjour, monde !"
+print "Je le redis une seconde fois : bonjour Monde !"
+print "C'est cool de parler, allez, je m'en vais !"
---
+++ my_first_program/my_seventh_program.rb
@@ -0,0 +1,3 @@
+# my_seventh_program.rb
+hello_string = "Bonjour, Monde !"
+puts hello_string
